dbf303bead651efb365ab876d0220febaf056bd1
Jaylan Jones <jaylanjones421@gmail.com>
2018-01-15 21:13:18 -0600

    finished mini project!

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors"),
+  massive = require("massive");
+require("dotenv").config();
+const controller = require('./controller')
 
 const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
+app.use(bodyParser.json());
+app.use(cors());
+massive(process.env.CONNECTION_STRING).then(dbInstance=>{
+      app.set('db',dbInstance);
 
-const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+      // dbInstance.new_planes()
+      // .then(planes=>console.log(planes))
+      // .catch(err=>console.log(err))
+      // dbInstance.get_planes()
+      // .then(planes=>console.log(planes))
+      // .catch(err=>console.log(planes))
+});
+app.get('/api/planes/', controller.getPlanes);
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
